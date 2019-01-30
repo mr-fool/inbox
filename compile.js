99bc50f94d0942bc060c949d9db2a7774ad3f158
mr-fool <mr-fool@users.noreply.github.com>
2019-01-29 17:03:41 -0700

    Add files via upload

---
+++ compile.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+
+const inboxPath = path.resolve(__dirname,'contracts','Indox.sol');
+const source = fs.readFileSync(indoxPath, 'utf8');
+

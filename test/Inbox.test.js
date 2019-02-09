640ffa6b3afc387378028823f33952344df9bf15
mr-fool <mr-fool@users.noreply.github.com>
2019-02-09 12:34:00 -0700

    missing quotation mark

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(Web3.providers.HttpProvider(http://localhost:8545"));
+const web3 = new Web3(Web3.providers.HttpProvider("http://localhost:8545"));
 
 
 beforeEach( () => {

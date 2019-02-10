18c6bb3f599b055e3641f1c086e44cddc198ee5e
mr-fool <mr-fool@users.noreply.github.com>
2019-02-10 00:02:16 -0700

    error

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,17 +9,24 @@ const web3 = new Web3(Web3.providers.HttpProvider(
 const { interface, bytyecode } = require('../compile');
 
 let accounts;
+let inbox;
 beforeEach( async () => {
 //Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
 //Use one of those accounts to deploy 
 //the contract
+    inbox = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: '0x' + compiledFactory.bytecode, arguments: ['cyka blyat'] })
+    .send({ gas: '1000000', from: accounts[0] });
+    /*await new web3.eth.Contract(JSON.parse(interface))
+        .deploy(   {data: bytecode, arguments: ['cyka blyat'] })
+        .send({ from: accounts[0], gas: '1000000' })*/
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     } );
 });

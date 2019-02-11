0369d3535f7c652631becc140fa94dbf438d8d57
mr-fool <mr-fool@users.noreply.github.com>
2019-02-11 13:27:28 -0700

    Update Inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -11,13 +11,14 @@ const { abi, evm } = require('../compile');
 let accounts;
 let inbox;
 
+console.log(abi);
+
 beforeEach( async () => {
 //Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
 //Use one of those accounts to deploy 
 //the contract
-    console.log(abi);
     inbox = await new web3.eth.Contract(abi)
         .deploy(   {data:  evm.bytecode.object, arguments: ['cyka blyat'] })
         .send({ from: accounts[0], gas: '1000000' })

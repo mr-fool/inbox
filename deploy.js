610c00f59441c1bce6abfd792b76cfd2c70ea866
mr-fool <mr-fool@users.noreply.github.com>
2019-02-12 13:49:14 -0700

    added async await again

--- deploy.js
+++ deploy.js
@@ -10,11 +10,11 @@ const provider = new HDWalletProvider (
 
 const web3 = new Web3(provider);
 
-const deploy = () => {
-    const accounts = web3.eth.getAccounts();
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = new web3.eth.Contract(abi)
+    const result = await new web3.eth.Contract(abi)
     .deploy({data: evm.bytecode.object , arguments: ['Hi there!']}) // add 0x bytecode
     .send({from: accounts[0], gas: '1000000'}); // remove 'gas'
 
